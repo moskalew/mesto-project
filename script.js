dc8704efa728431a529dfbee1c3515632fc482f4
Andrej Moskalew <moskalev.web.dev@gmail.com>
2023-01-09 12:17:33 +0300

    u-la-la

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
+let popup = document.querySelector('.popup');
 let btnEditProfile = document.querySelector('.profile__edit-button');
 let closeIcon = document.querySelector('.popup__close-icon');
-let popup = document.querySelector('.popup');
 
 function popupOpen() {
     popup.classList.add('popup_opened'); 
