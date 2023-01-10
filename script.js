74a15fa3088399cdc89bdf945f722f530ac1508c
Andrej Moskalew <moskalev.web.dev@gmail.com>
2023-01-11 02:15:17 +0300

    функционал popup

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
-let popup = document.querySelector('.popup');
-let btnEditProfile = document.querySelector('.profile__edit-button');
-let closeIcon = document.querySelector('.popup__close-icon');
+const popup = document.querySelector('.popup');
+const btnEditProfile = document.querySelector('.profile__edit-button');
+const closeIcon = document.querySelector('.popup__close-icon');
+const profileForm = popup.querySelector('[name="form"]');
+
+const profileInfo = {
+    name: document.querySelector('.profile__name').textContent,
+    mission: document.querySelector('.profile__mission').textContent,
+}
 
 function popupOpen() {
     popup.classList.add('popup_opened'); 
+    setProfileInfo(profileInfo.name, profileInfo.mission);
 }
-btnEditProfile.addEventListener('click', popupOpen);
 
 function popupClose() {
     popup.classList.toggle('popup_opened');  
 }
+
+function setProfileInfo(name, mission) {
+    profileForm.elements.name.value = name;
+    profileForm.elements.mission.value = mission;
+}
+
+function saveProfile(event) {
+    event.preventDefault();
+    // console.log(event);
+    profileInfo.name = event.target.elements.name.value;
+    profileInfo.mission = event.target.elements.mission.value;
+    document.querySelector('.profile__name').textContent = profileInfo.name;
+    document.querySelector('.profile__mission').textContent = profileInfo.mission;
+    popupClose();
+}
+
+btnEditProfile.addEventListener('click', popupOpen);
 closeIcon.addEventListener('click', popupClose);
+profileForm.addEventListener('submit', saveProfile);
+
